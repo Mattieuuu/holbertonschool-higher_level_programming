9920112428ec396c565894b524b35248c8793e58
Mattieuuu <@mattieu.mx@gmail.com>
2025-01-15 13:01:10 +0100

    2

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_f_3 = word[:3]
-word_l_2 = word[2:]
-mid_word = word[1:-1]
-print(f"First 3 letters : {word_f_3}")
-print(f"Last 2 letters: {word_l_2}")
-print(f"Middle word : {mid_word}")
+word_first_3 = word[:3]
+word_last_2 = word[2:]
+middle_word = word[1:-1]
+print(f"First 3 letters : {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word : {middle_word}")
