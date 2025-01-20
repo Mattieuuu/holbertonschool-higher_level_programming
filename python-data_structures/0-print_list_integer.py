68c7dde947618acc2290cce382fb9b5ec5644737
Mattieuuu <@mattieu.mx@gmail.com>
2025-01-20 10:22:27 +0100

    0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for element in my_list:
+        print("{}".format(element))  
+# Utiliser str.format() pour afficher
+# chaque élément de la liste sur une nouvelle ligne
