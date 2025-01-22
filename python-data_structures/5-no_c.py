fa245d3d9ce8dd877360dc0047b7e5eff6ee1881
Mattieuuu <@mattieu.mx@gmail.com>
2025-01-22 12:08:37 +0100

    0

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string+=char
-            return new_string
+            new_string += char
+    return new_string
