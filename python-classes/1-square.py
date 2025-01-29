e6d4791f0ffacea8dd69269e78c6d8f6ac2ec6ce
Mattieuuu <@mattieu.mx@gmail.com>
2025-01-29 13:36:25 +0100

    2

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module for Class Square
+'''
+
+
+class Square:
+    '''Class Square'''
+    def __init__(self, size):
+        ''' Init square with a given size.
+        Args:
+            size: Size of the square
+        '''
+        self.__size = size
