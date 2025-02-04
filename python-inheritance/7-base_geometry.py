8c585cab8598440f62da517943ac60a2a9455eb4
Mattieuuu <@mattieu.mx@gmail.com>
2025-02-04 16:27:56 +0100

    push task 0 to 6

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,16 +4,15 @@
 
 
 class BaseGeometry():
-    """ BaseGeometry class """
+    """BaseGeometry class"""
 
     def area(self):
-        """ raises an Exception """
-        raise Exception('area() is not implemented')
+        """Raise an Exception"""
+        raise Exception("area() is not implemented")
 
-
-def integer_validator(self, name, value):
-    """Valid value"""
-    if type(value) is not int:
-        raise TypeError(name + " must be an integer")
-    if value <= 0:
-        raise ValueError(name + " must be greater than 0")
+    def integer_validator(self, name, value):
+        """Validate value"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
