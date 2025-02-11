ca1d3cda6dac8071b772c192376af945e6a013c8
Mattieuuu <@mattieu.mx@gmail.com>
2025-02-11 08:40:24 +0100

    push task 0 to 3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 def read_file(filename=""):
 
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
+    with open(filename, encoding='utf-8') as f:
+        content = f.read()
         print(content)
+        f.closed
