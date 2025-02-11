cc1e71198cf8c2394885e94ac2d0baaae9e93383
Mattieuuu <@mattieu.mx@gmail.com>
2025-02-11 15:12:47 +0100

    a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,5 +4,6 @@
 
 
 def append_write(filename="", text=""):
+    """Append a string at the end of a text file (UTF-8)"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
