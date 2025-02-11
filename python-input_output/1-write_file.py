7ce774d6294415ac5eaf158f83b8d1b7b77dbf92
Mattieuuu <@mattieu.mx@gmail.com>
2025-02-11 15:04:06 +0100

    push all task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,9 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    # Ouvre le fichier en mode écriture ('w'), cela écrase le contenu existant ou crée le fichier si nécessaire
     with open(filename, 'w', encoding='utf-8') as file:
-        # Écrit le texte dans le fichier
         file.write(text)
-        # Retourne le nombre de caractères écrits
         return len(text)
