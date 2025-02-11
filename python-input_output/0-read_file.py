228269ffee403dbd72ad63fb5e5acbd0f084a0d7
Mattieuuu <@mattieu.mx@gmail.com>
2025-02-11 08:35:16 +0100

    push task 0 to 3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    # Ouvre le fichier en mode lecture ('r') en utilisant la construction 'with'
+    # Ouvre le fichier en mode lecture ('r') en utilisant 'with'
     with open(filename, 'r', encoding='utf-8') as file:
         # Lit tout le contenu du fichier
         content = file.read()
