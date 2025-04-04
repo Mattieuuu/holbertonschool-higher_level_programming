646426eb435de386780fa00a27d2bb696135f11a
Mattieuuu <mattieu.mx@gmail.com>
2025-04-04 16:53:34 +0200

    aaa

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+const characterDiv = document.querySelector('#character');
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    characterDiv.textContent = data.name;
+  })
+  .catch(error => console.error('Error fetching character:', error));
